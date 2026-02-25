d2f14f95161d319c364ee19a202327e37f4d2978
Dimas Rizal Ramadhani <dimasrizal489@gmail.com>
2026-02-25 11:19:02 +0700

    fix refactor modul3

--- lib/features/auth/login_view.dart
+++ lib/features/auth/login_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // Import Controller milik sendiri (masih satu folder)
 import 'package:logbook_app_001/features/auth/login_controller.dart';
 // Import View dari fitur lain (Logbook) untuk navigasi
-import 'package:logbook_app_001/features/logbook/counter_view.dart';
+import 'package:logbook_app_001/features/logbook/log_view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -30,7 +30,7 @@ class _LoginViewState extends State<LoginView> {
       case LoginResult.success:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => CounterView(username: user)),
+            MaterialPageRoute(builder: (context) => const LogView()),
         );
         return;
 
